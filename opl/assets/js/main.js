d7653115fac1ded5583c74e809ca3f51e25d843a
tonynguyen176 <75963273+tonynguyen176@users.noreply.github.com>
2021-09-01 17:58:28 +0700

    Change jackpot value

--- opl/assets/js/main.js
+++ opl/assets/js/main.js
@@ -31,7 +31,7 @@
     /* ==========================================================================
     countdown timer
     ========================================================================== */
-     jQuery('#clock').countdown('2021/09/04',function(event){
+     jQuery('#clock').countdown('2021/09/11',function(event){
       var $this=jQuery(this).html(event.strftime(''
       +'<div class="time-entry days"><span>%-D</span> Days</div> '
       +'<div class="time-entry hours"><span>%H</span> Hours</div> '
